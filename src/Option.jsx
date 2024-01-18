f0fcb27c249b96f6b7d25d5f068c03da6bdc5e4e
emongo <emongochadrack@gmail.com>
2024-01-18 16:26:20 +0200

    ajout de gap

--- src/Option.jsx
+++ src/Option.jsx
@@ -3,7 +3,7 @@ import { Icones } from './Icones'
 
 export default function Option(props) {
     return (
-        <div className='flex mb-5'>
+        <div className='flex mb-5 gap-6'>
             <Icones icones={props.icones} />
             <h3> {props.name} </h3>
         </div>
