215fb28ef790a475ba7427a16d597e729c80f2e6
emongo <emongochadrack@gmail.com>
2024-01-18 12:11:23 +0200

    ajout du flex sur le menu

--- src/Option.jsx
+++ src/Option.jsx
@@ -3,7 +3,7 @@ import { Icones } from './Icones'
 
 export default function Option(props) {
     return (
-        <div>
+        <div className='flex'>
             <Icones icones={props.icones} />
             <h3> {props.name} </h3>
         </div>
