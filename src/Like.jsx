f555ff03ff48087e659ca3b1cb8b67ddffa66637
emongo <emongochadrack@gmail.com>
2024-01-27 07:45:15 +0200

    ajout de l'icone like et l'incrementation

---
+++ src/Like.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import { useState } from 'react'
+
+export default function Like({ }) {
+    const [imageClick, setImageClick] = useState(false);
+    const [like, setLike] = useState(0);
+    const handleclick = () => {
+        setImageClick(!imageClick);
+        imageClick ? setLike(like - 1) : setLike(like + 1);
+    }
+    const likeIcone = imageClick ? "src/assets/images/icons8-costume-de-coeur-emoji-96.png" : "src/assets/images/React.svg"
+    return (
+        <div>
+            <div className='rounded-full w-6'>
+                <img onClick={handleclick} src={likeIcone} alt='' />
+            </div>
+            <span> {like} </span>
+        </div>
+    )
+}
